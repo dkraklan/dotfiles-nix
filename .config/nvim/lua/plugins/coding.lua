799ada4f16589c9877544e75f208663c6976e0d8
dkraklan <dylan@dkraklan.me>
2024-05-09 19:16:08 -0700

    rebase i think idfk

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,16 @@
 local plugins = {
-    { 'echasnovski/mini.comment', version = '*' },
+    { "echasnovski/mini.comment", version = "*" },
+    {
+        "f-person/git-blame.nvim",
+        config = function()
+            require("gitblame").setup({
+                enabled = true,
+                message_template = "<summary> • <date>",
+                message_when_not_committed = "uncommited !",
+                virtual_text_column = 80,
+            })
+        end,
+    },
 }
 
 return plugins
--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -7,147 +7,148 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-	-- NOTE: Yes, you can install new plugins here!
-	"mfussenegger/nvim-dap",
-	-- NOTE: And you can specify dependencies as well
-	dependencies = {
-		-- Creates a beautiful debugger UI
-		"rcarriga/nvim-dap-ui",
+    -- NOTE: Yes, you can install new plugins here!
+    "mfussenegger/nvim-dap",
+    -- NOTE: And you can specify dependencies as well
+    dependencies = {
+        -- Creates a beautiful debugger UI
+        "rcarriga/nvim-dap-ui",
 
-		-- Required dependency for nvim-dap-ui
-		"nvim-neotest/nvim-nio",
+        -- Required dependency for nvim-dap-ui
+        "nvim-neotest/nvim-nio",
 
-		-- Installs the debug adapters for you
-		"williamboman/mason.nvim",
-		"jay-babu/mason-nvim-dap.nvim",
+        -- Installs the debug adapters for you
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
 
-		-- Add your own debuggers here
-		"leoluz/nvim-dap-go",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+        -- Add your own debuggers here
+        "leoluz/nvim-dap-go",
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
 
-		require("mason-nvim-dap").setup({
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
-			automatic_setup = true,
+        require("mason-nvim-dap").setup({
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_setup = true,
 
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
-			handlers = {},
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
 
-			-- You'll need to check that you have the required things installed
-			-- online, please don't ask me how to install them :)
-			ensure_installed = {
-				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
-				"godot",
-			},
-		})
+            -- You'll need to check that you have the required things installed
+            -- online, please don't ask me how to install them :)
+            ensure_installed = {
+                -- Update this to ensure that you have the debuggers for the langs you want
+                "delve",
+                "godot",
+            },
+        })
 
-		-- godot debug shit
-		dap.adapters.godot = {
-			type = "server",
-			host = "127.0.0.1",
-			port = 6006,
-		}
+        -- godot debug shit
+        dap.adapters.godot = {
+            type = "server",
+            host = "127.0.0.1",
+            port = 6006,
+        }
 
-		dap.configurations.gdscript = {
-			{
-				type = "godot",
-				request = "launch",
-				name = "Launch Scene",
-				project = "${workspaceFolder}",
-				launch_scene = true,
-			},
-		}
+        dap.configurations.gdscript = {
+            {
+                type = "godot",
+                request = "launch",
+                name = "Launch Scene",
+                project = "${workspaceFolder}",
+                launch_scene = true,
+            },
+        }
 
-		-- Basic debugging keymaps, feel free to change to your liking!
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-		vim.keymap.set("n", "<F6>", function()
-			dap.terminate()
-			dapui.close()
-		end, { desc = "Terminate DAP and close DAPUI" })
-		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		vim.keymap.set("n", "<leader>B", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end, { desc = "Debug: Set Breakpoint" })
+        -- Basic debugging keymaps, feel free to change to your liking!
 
-		-- Dap UI setup
-		dapui.setup({
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
-			layouts = {
-				{
-					elements = {
-						{
-							id = "scopes",
-							size = 0.25,
-						},
-						{ id = "breakpoints", size = 0.25 },
-						{ id = "stacks", size = 0.25 },
-						{ id = "watches", size = 0.25 },
-					},
-					position = "right",
-					size = 40,
-				},
-				{
-					elements = {
-						{
-							id = "repl",
-							size = 0.2,
-						},
-						{
-							id = "console",
-							size = 0.5,
-						},
-					},
-					position = "bottom",
-					size = 20,
-				},
-			},
-		})
+        vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+        vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
+        vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+        vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+        vim.keymap.set("n", "<F6>", function()
+            dap.terminate()
+        end, { desc = "Terminate DAP session" })
 
-		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+        vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+        vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+        vim.keymap.set("n", "<leader>B", function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end, { desc = "Debug: Set Breakpoint" })
 
-		--dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		--dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		--dap.listeners.before.event_exited["dapui_config"] = dapui.close
+        -- Dap UI setup
+        dapui.setup({
+            expand_lines = false,
+            icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+            controls = {
+                icons = {
+                    pause = "⏸",
+                    play = "▶",
+                    step_into = "⏎",
+                    step_over = "⏭",
+                    step_out = "⏮",
+                    step_back = "b",
+                    run_last = "▶▶",
+                    terminate = "⏹",
+                    disconnect = "⏏",
+                },
+            },
+            layouts = {
+                {
+                    elements = {
+                        {
+                            id = "console",
+                            size = 0.2,
+                        },
+                        {
+                            id = "stacks",
+                            size = 0.2,
+                        },
+                        {
+                            id = "watches",
+                            size = 0.2,
+                        },
+                        {
+                            id = "scopes",
+                            size = 0.2,
+                        },
+                        {
+                            id = "breakpoints",
+                            size = 0.2,
+                        },
+                    },
+                    position = "bottom",
+                    size = 20,
+                },
+            },
+        })
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.after.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 
-		-- Install golang specific config
-		require("dap-go").setup()
-	end,
+        --dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+        --dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+        --dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.after.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        -- Install golang specific config
+        require("dap-go").setup()
+    end,
 }
