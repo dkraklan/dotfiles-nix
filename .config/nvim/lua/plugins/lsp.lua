e145561f3bcfd2c9f58a83448070feee028f01e6
dkraklan <dylan@dkraklan.me>
2024-05-11 13:24:24 -0700

    testing inlay hints, formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,51 +1,70 @@
 local plugins = {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			-- lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gdscript.setup({
-				capabilities = capabilities,
-			})
-      lspconfig.ansiblels.setup({
-        capabilities = capabilities,
-      })
-			vim.keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "Show tooltip hint" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.gdformat,
-				},
-			})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            -- inlay hints
+            vim.g.inlay_hints_visible = true
+            local on_attach = function(client, bufnr)
+                -- if client.server_capabilities.inlayHintProvider then
+                --     vim.g.inlay_hints_visible = true
+                --     vim.lsp.inlay_hint(bufnr, true)
+                -- else
+                --     print("no inlay hints available")
+                -- end
+            end
+            -- lua
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        hint = { enable = true },
+                    },
+                },
+            })
+            lspconfig.gdscript.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+            lspconfig.ansiblels.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+            vim.keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "Show tooltip hint" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "Go to references" })
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.gdformat,
+                },
+            })
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
+        end,
+    },
 }
 
 return plugins
