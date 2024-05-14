36ffdb810f77e8c4dc62c758e093cfb3ff6b14bb
dkraklan <dylan@dkraklan.me>
2024-05-13 19:35:59 -0700

    formatting

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,121 +1,123 @@
 local plugins = {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				auto_install = true,
-				ensure_installed = {
-					"vim",
-					"vimdoc",
-					"lua",
-					"gdscript",
-					"godot_resource",
-					"gdshader",
-					"python",
-					"markdown",
-					"markdown_inline",
-          "yaml",
-				},
-				highlight = { enable = true },
-				indent = { enabled = false },
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			enable_git_status = true,
-			git_status = {
-				symbols = {
-					-- Change type
-					added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-					modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-					deleted = "✖", -- this can only be used in the git_status source
-					renamed = "󰁕", -- this can only be used in the git_status source
-					-- Status type
-					untracked = "",
-					ignored = "",
-					unstaged = "󰄱",
-					staged = "",
-					conflict = "",
-				},
-			},
-			name = {
-				trailing_slash = false,
-				use_git_status_colors = true,
-				highlight = "NeoTreeFileName",
-			},
-			sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-			open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-			filesystem = {
-				bind_to_cwd = false,
-				follow_current_file = { enabled = true },
-				use_libuv_file_watcher = true,
-			},
-			window = {
-				mappings = {
-					["<space>"] = "none",
-					["Y"] = {
-						function(state)
-							local node = state.tree:get_node()
-							local path = node:get_id()
-							vim.fn.setreg("+", path, "c")
-						end,
-						desc = "Copy Path to Clipboard",
-					},
-					["O"] = {
-						function(state)
-							require("lazy.util").open(state.tree:get_node().path, { system = true })
-						end,
-						desc = "Open with System Application",
-					},
-				},
-			},
-			default_component_configs = {
-				indent = {
-					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-					expander_collapsed = "",
-					expander_expanded = "",
-					expander_highlight = "NeoTreeExpander",
-				},
-			},
-		},
-		config = function(_, opts)
-			vim.keymap.set("n", "<leader>e", ":Neotree focus<cr>", { desc = "Focus neotree" })
-			vim.keymap.set("n", "<C-e>", ":Neotree toggle<cr>", { desc = "Toggle Neotree" })
-			require("neo-tree").setup(opts)
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                auto_install = true,
+                ensure_installed = {
+                    "vim",
+                    "vimdoc",
+                    "lua",
+                    "gdscript",
+                    "godot_resource",
+                    "gdshader",
+                    "python",
+                    "markdown",
+                    "markdown_inline",
+                    "yaml",
+                    "kdl",
+                },
+                highlight = { enable = true },
+                indent = { enabled = false },
+            })
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            enable_git_status = true,
+            git_status = {
+                symbols = {
+                    -- Change type
+                    added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                    modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                    deleted = "✖", -- this can only be used in the git_status source
+                    renamed = "󰁕", -- this can only be used in the git_status source
+                    -- Status type
+                    untracked = "",
+                    ignored = "",
+                    unstaged = "󰄱",
+                    staged = "",
+                    conflict = "",
+                },
+            },
+            name = {
+                trailing_slash = false,
+                use_git_status_colors = true,
+                highlight = "NeoTreeFileName",
+            },
+            sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+            open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+            filesystem = {
+                bind_to_cwd = false,
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+            },
+            window = {
+                mappings = {
+                    ["<space>"] = "none",
+                    ["Y"] = {
+                        function(state)
+                            local node = state.tree:get_node()
+                            local path = node:get_id()
+                            vim.fn.setreg("+", path, "c")
+                        end,
+                        desc = "Copy Path to Clipboard",
+                    },
+                    ["O"] = {
+                        function(state)
+                            require("lazy.util").open(state.tree:get_node().path, { system = true })
+                        end,
+                        desc = "Open with System Application",
+                    },
+                },
+            },
+            default_component_configs = {
+                indent = {
+                    with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+            },
+        },
+        config = function(_, opts)
+            vim.keymap.set("n", "<leader>e", ":Neotree focus<cr>", { desc = "Focus neotree" })
+            vim.keymap.set("n", "<C-e>", ":Neotree toggle<cr>", { desc = "Toggle Neotree" })
+            require("neo-tree").setup(opts)
+        end,
+    },
 }
 
 return plugins
