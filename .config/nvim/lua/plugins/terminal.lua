cd90c1811ec153ccbddaf0aae51b43a76a3b3044
dkraklan <dylan@dkraklan.me>
2024-05-15 13:40:34 -0700

    terminal shit

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -1,28 +1,28 @@
 local plugins = {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {},
-		config = function()
-			local Terminal = require("toggleterm.terminal").Terminal
-			local lazygit = Terminal:new({
-				cmd = "lazygit",
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {},
+        config = function()
+            local Terminal = require("toggleterm.terminal").Terminal
+            local lazygit = Terminal:new({
+                cmd = "lazygit",
                 direction = "float",
-				hidden = false,
-			})
+                hidden = false,
+            })
 
-			function _lazygit_toggle()
-				lazygit:toggle()
-			end
+            function _lazygit_toggle()
+                lazygit:toggle()
+            end
 
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>gg",
-				"<cmd>lua _lazygit_toggle()<CR>",
-				{ noremap = true, silent = true }
-			)
-		end,
-	},
+            vim.api.nvim_set_keymap(
+                "n",
+                "<leader>gg",
+                "<cmd>lua _lazygit_toggle()<CR>",
+                { noremap = true, silent = true }
+            )
+        end,
+    },
 }
 
 return plugins
