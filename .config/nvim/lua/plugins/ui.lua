8f5b425fe4a54d2d9a33c671b2cb55e4a049d3c6
dkraklan <dylan@dkraklan.me>
2024-05-29 20:12:37 -0700

    add one directory up to lualine filename

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -25,7 +25,27 @@ local plugins = {
                 sections = {
                     lualine_a = { "mode" },
                     lualine_b = { "branch", "diff", "diagnostics" },
-                    lualine_c = { "filename" },
+                    lualine_c = {
+                        {
+                            'filename',
+                            file_status = true, -- Displays file status (readonly status, modified status)
+                            newfile_status = false, -- Display new file status (new file means no write after created)
+                            path = 4, -- 0: Just the filename
+                            -- 1: Relaitive path
+                            -- 2: Absolute path
+                            -- 3: Absolute path, with tilde as the home directory
+                            -- 4: Filename and parent dir, with tilde as the home directory
+
+                            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                            -- for other components. (terrible name, any suggestions?)
+                            symbols = {
+                                modified = '[+]', -- Text to show when the file is modified.
+                                readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                                newfile = '[New]', -- Text to show for newly created file before first write
+                            }
+                        }
+                    },
                     lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
                     lualine_y = { "progress" },
                     lualine_z = { "location" },
