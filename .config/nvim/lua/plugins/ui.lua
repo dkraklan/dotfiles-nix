737427309e8fc074512b6db01332f3f6afd46062
dkraklan <dylan@dkraklan.me>
2024-05-30 00:55:34 -0700

    add parent dir to inactive statusbar

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -28,9 +28,9 @@ local plugins = {
                     lualine_c = {
                         {
                             'filename',
-                            file_status = true, -- Displays file status (readonly status, modified status)
+                            file_status = true,     -- Displays file status (readonly status, modified status)
                             newfile_status = false, -- Display new file status (new file means no write after created)
-                            path = 4, -- 0: Just the filename
+                            path = 4,               -- 0: Just the filename
                             -- 1: Relaitive path
                             -- 2: Absolute path
                             -- 3: Absolute path, with tilde as the home directory
@@ -39,10 +39,10 @@ local plugins = {
                             shorting_target = 40, -- Shortens path to leave 40 spaces in the window
                             -- for other components. (terrible name, any suggestions?)
                             symbols = {
-                                modified = '[+]', -- Text to show when the file is modified.
-                                readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                                modified = '[+]',      -- Text to show when the file is modified.
+                                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
                                 unnamed = '[No Name]', -- Text to show for unnamed buffers.
-                                newfile = '[New]', -- Text to show for newly created file before first write
+                                newfile = '[New]',     -- Text to show for newly created file before first write
                             }
                         }
                     },
@@ -53,7 +53,27 @@ local plugins = {
                 inactive_sections = {
                     lualine_a = {},
                     lualine_b = {},
-                    lualine_c = { "filename" },
+                    lualine_c = {
+                        {
+                            'filename',
+                            file_status = true,     -- Displays file status (readonly status, modified status)
+                            newfile_status = false, -- Display new file status (new file means no write after created)
+                            path = 4,               -- 0: Just the filename
+                            -- 1: Relaitive path
+                            -- 2: Absolute path
+                            -- 3: Absolute path, with tilde as the home directory
+                            -- 4: Filename and parent dir, with tilde as the home directory
+
+                            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                            -- for other components. (terrible name, any suggestions?)
+                            symbols = {
+                                modified = '[+]',      -- Text to show when the file is modified.
+                                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                                newfile = '[New]',     -- Text to show for newly created file before first write
+                            }
+                        }
+                    },
                     lualine_x = { "location" },
                     lualine_y = {},
                     lualine_z = {},
