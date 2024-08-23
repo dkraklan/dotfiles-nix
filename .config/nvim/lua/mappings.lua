95d80208704e5cb924cf272c9df307751e608b85
dkraklan <dylan@dkraklan.me>
2024-08-22 20:07:41 -0700

    Update rest.nvim to use opts instead of setup

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -26,3 +26,8 @@ map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 map('n', 'ge', vim.diagnostic.open_float, {desc = "Show diagnostics in floating window"})
+
+-- Rest.nvim
+vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Run rest command" })
+vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>", { desc = "Run last rest command" })
+
