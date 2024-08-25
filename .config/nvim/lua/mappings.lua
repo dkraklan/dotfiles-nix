012348f7869a9e803816e75ebfff1961fcb6b79c
dkraklan <dylan@dkraklan.me>
2024-08-25 11:09:51 -0700

    Add floating term mapping and rest mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,5 +1,8 @@
 -- my keybindings
 local map = vim.keymap.set
+local Terminal = require("toggleterm.terminal").Terminal
+local utils = require("utils")
+
 
 map("n", "<leader>w", ":w!<cr>", { desc = "Write Force" })
 map("n", "<leader>q", ":q!<cr>", { desc = "Quit force" })
@@ -25,9 +28,27 @@ map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-map('n', 'ge', vim.diagnostic.open_float, {desc = "Show diagnostics in floating window"})
+map("n", "ge", vim.diagnostic.open_float, { desc = "Show diagnostics in floating window" })
 
 -- Rest.nvim
 vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Run rest command" })
 vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>", { desc = "Run last rest command" })
+vim.keymap.set("n", "<leader>ro", "<cmd>hori Rest open<CR>", { desc = "Open rest results window" })
 
+-- open a floating terminal
+local _floating_term = Terminal:new({
+    dir = utils.find_project_root(), -- the working directory
+    direction = "float",          -- the direction, can be: 'vertical', 'horizontal', 'window', 'float'
+    hidden = false,
+    close_on_exit = false,
+    float_opts = { border = "single" }, -- table: border, width, height, winblend, highlights
+    on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+    on_close = function(term)
+        vim.cmd("redraw!")
+    end,
+    auto_scroll = true, -- if true, the terminal will scroll on new output
+})
+
+vim.keymap.set("n", "<leader>tt", function() _floating_term:toggle() end, { desc = "Open floating terminal" })
