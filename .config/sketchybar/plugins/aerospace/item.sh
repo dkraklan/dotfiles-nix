646ef7a7ca8a9de22262ef71fea04a24c64dfec4
Dylan <dylan@dkraklan.me>
2024-12-16 22:07:15 -0500

    sketchybar aerospace tweaks and fix cpu

--- .config/sketchybar/plugins/aerospace/item.sh
+++ .config/sketchybar/plugins/aerospace/item.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env zsh
 
 #### Aerospace ####
 sketchybar --add event aerospace_workspace_change
+
+echo "aerospace item.sh"
+
 divider=(
 	background.color="$SURFACE0"
 	background.border_color="$SURFACE1"
@@ -8,7 +12,57 @@ divider=(
 	background.padding_left=5
 	background.padding_right=10
 )
+
+# get_icon_for_app() {
+#     case $1 in
+#         "Microsoft Edge") icon="" ;;
+#         "iTerm2") icon="" ;;
+#         "Telegram") icon="" ;;
+#         "Discord") icon="󰙯" ;;
+#         "Slack") icon="" ;;
+#         "DBeaver") icon="" ;;
+#         *) icon="" ;;
+#     esac
+#     echo $icon
+# }
+
 for sid in $(aerospace list-workspaces --all); do
+    # echo "sid: $sid"
+    # icon=""
+    # cur_windows=$(aerospace list-windows --workspace $sid --format "%{app-name}")
+
+    # while IFS= read -r app; do
+    #     echo "app: $app"
+    #     icon+=$(get_icon_for_app "$app")
+    #     # if we already have an icon, add a space
+    #     if [ ! -z "$icon" ]; then
+    #         icon+=" "
+    #     fi
+    #
+    #     echo "icon: $icon"
+    # done <<< "$cur_windows"
+
+    # case $sid in 
+    #     "1")
+    #         icon=""
+    #         ;;
+    #     "2")
+    #         icon="󰭹"
+    #         ;;
+    #     "3")
+    #         icon=""
+    #         ;;
+    #     "5")
+    #         icon=""
+    #         ;;
+    #     "6")
+    #         icon="󰎚"
+    #         ;;
+    #     *)
+    #         icon=""
+    #         ;;
+    # esac 
+
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
@@ -17,13 +71,14 @@ for sid in $(aerospace list-workspaces --all); do
         background.height=20 \
         background.drawing=off \
         label="$sid" \
+        label.align=left \
         label.padding_right=8 \
         label.padding_left=0 \
         click_script="aerospace workspace $sid" \
-        script="$PLUGIN_DIR/aerospace/scripts/aerospace.sh $sid"
-
-
+        script="$PLUGIN_DIR/aerospace/scripts/aerospace.sh $sid" \
+        # update_freq=1
 done
+
 
 
 sketchybar --add bracket spaces space.1 space.2 space.3 space.4 space.5 space.6 space.7 space.8 space.9 \
--- .config/sketchybar/plugins/aerospace/scripts/aerospace.sh
+++ .config/sketchybar/plugins/aerospace/scripts/aerospace.sh
@@ -2,11 +2,63 @@
 
 source $HOME/.config/sketchybar/colors.sh
 
+echo "aerospace script"
+
+
+
+get_icon_for_app() {
+    case $1 in
+        "Microsoft Edge") icon="" ;;
+        "iTerm2") icon="" ;;
+        "Telegram") icon="" ;;
+        "Discord") icon="󰙯" ;;
+        "Slack") icon="" ;;
+        "DBeaver") icon="" ;;
+        "Signal") icon="󰭹" ;;
+        "Obsidian") icon="󱞁" ;;
+        "Spotify") icon="" ;;
+        *) icon="" ;;
+    esac
+    echo $icon
+}
+
+sid=$(echo "$NAME" | cut -d'.' -f2)
+
+echo "updating aerospace $sid"
+
+icon=""  # Initialize the icon variable as empty
+cur_windows=$(aerospace list-windows --workspace $sid --format "%{app-name}")
+echo "cur_windows: $cur_windows"
+
+while IFS= read -r app; do
+    echo "app: $app"
+    app_icon=$(get_icon_for_app "$app")  # Fetch the icon for this app
+    echo "found app icon: $app_icon" 
+    if [ -n "$app_icon" ]; then  # Only add non-empty icons
+        if [ -z "$icon" ]; then
+            echo "first app icon: $app_icon"
+            icon="$app_icon"  # First app, no space before the icon
+        else
+            echo "app icon: $app_icon"
+            icon+=" $app_icon"  # Append with a space before each subsequent icon
+        fi
+    fi
+done <<< "$cur_windows"
+
+echo "icon: $icon"
+
+
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on \
         background.color=$ROSEWATER\
-        label.color=$BASE
+        label.color=$BASE\
+        icon.color=$BASE\
+        icon="$icon" 
 else
     sketchybar --set $NAME background.drawing=off \
-        label.color=$TEXT
+        label.color=$SAPPHIRE\
+        icon.color=$TEXT\
+        icon="$icon" 
+
 fi
